441d00bac5f68e1fe82e9641cacfb28efc385de8
Sergio <71512161+SergJprog@users.noreply.github.com>
2023-05-16 09:47:34 -0600

    actualizacion de formulario

---
+++ funciones/formulario.js
@@ -0,0 +1,26 @@
+let mail = document.getElementById("mail").style.display = 'none'
+
+
+function selectRadio() {
+
+    const radios = document.getElementsByName('contactado-por');
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+
+            if(radios[i].value === "telefono"){
+                let telefono = document.getElementById("tele").style.display = "block"
+                let mail = document.getElementById("mail").style.display = 'none'
+            }else if (radios[i].value === "Whatsapp"){
+                let telefono = document.getElementById("tele").style.display = "block"
+                let mail = document.getElementById("mail").style.display = 'none'
+            }else {
+                let mail = document.getElementById("mail").style.display = "block"
+                let telefono = document.getElementById("tele").style.display = "none"
+            }
+            break;
+        }
+
+    }
+}
+
+
